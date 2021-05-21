f8fd8be48c39e899de5e49acf8093939cff47989
Phowg <77728886+Phowg@users.noreply.github.com>
2021-05-21 12:00:26 +0200

    Add files via upload
    Ultima entrega

---
+++ Script/Movimiento.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    [Tooltip("Velocidad de la bala")]
+    public float bulletSpeed;
+
+[Tooltip("enemimgo al que voy a matar")]
+    public GameObject enemy; 
+
+    
+
+    void Start()
+    {
+        // La bala se destruirá a los 3 segundos para evitar problemas de memoria
+        //Destroy(gameObject, 3);
+    }
+
+    void Update()
+    {
+        //comprueba si el enemigo existe o ya ha sido destruido
+        if (enemy != null)
+        {
+
+            // Movimiento de la bala en el eje Y según la velocidad especificada
+            transform.position = Vector3.MoveTowards(transform.position, enemy.transform.position, bulletSpeed);
+            //transform.Translate(0, bulletSpeed, 0);
+
+        }else
+        {
+            // si el enemigo se destruye se destruye la bala tambien
+            Destroy(gameObject);
+        }
+    }
+
+
+    
+
+    
+}
---
+++ Script/Tower.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Tooltip("Referencia al prefab de la bala")]
+    public GameObject bulletPrefab;
+
+    [Tooltip("Referencia al enemigo al que voy a disparar")]
+    public GameObject enemy;
+
+    public float height;
+
+   
+
+    
+
+    [Tooltip("frecuencia de disparo deseada, cada cuantos s se dispara")]
+    public float shootfrecuencia;
+
+    private float lastShotTime;
+
+
+
+    private void Update()
+    {
+        Shoot();
+    }
+    private void Shoot() { 
+        if (enemy != null)
+        {
+            //comprueba si ha pasado el tiempo correspondiente desde el último disparo
+            if (Time.time - lastShotTime >= shootfrecuencia)
+            {
+                //realiza el disparo instanciado la bala
+                GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
+                //Asigna a la bala instaciada el enemigo al que tiene que dirigirse
+                bala.GetComponent<Movimiento>().enemy = enemy;
+                //regista el momento en el que se ha disparado
+                lastShotTime = Time.time;
+            }
+        }
+
+
+    }
+
+  
+
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //combrueba si es el enemigo el que entra en el collaider
+        if (other.CompareTag("enemigo"))
+        {
+            enemy = other.gameObject;
+        }
+
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        enemy = null;
+    }
+
+}
